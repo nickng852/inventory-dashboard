61392a869d6df32473f0fcd77de3fa9124858c7a
Nick Ng <nickng@Nicks-MacBook-Pro.local>
2024-03-12 11:24:12 +0800

    layout modification

--- app/product/[productId]/edit/layout.tsx
+++ app/product/[productId]/edit/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="flex flex-col items-center p-6 md:p-10">
+        <section className="flex flex-col items-center p-4 md:p-10">
             {children}
         </section>
     )
