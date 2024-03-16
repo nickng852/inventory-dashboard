355bc951dc19bcf9ff2de070d497b8e7864c3ef2
Nick Ng <nickng@Nicks-MacBook-Pro.local>
2024-03-16 11:22:15 +0800

    fix nav item shown when not logged in

--- components/mobile-menu.tsx
+++ components/mobile-menu.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { SignedOut, SignUpButton, SignInButton } from '@clerk/nextjs'
 import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 
 import { navItems } from '@/components/nav-item'
@@ -14,7 +15,7 @@ import {
     SheetTrigger,
 } from '@/components/ui/sheet'
 
-export default function MobileMenu() {
+export default function MobileMenu({ userId }: { userId: string }) {
     const pathname = usePathname()
 
     return (
@@ -24,30 +25,59 @@ export default function MobileMenu() {
                     <HamburgerMenuIcon className="h-[1.2rem] w-[1.2rem]" />
                 </Button>
             </SheetTrigger>
-            <SheetContent side={'left'}>
+
+            <SheetContent
+                side={'left'}
+                className={`${!userId ? 'flex flex-col justify-end' : undefined}`}
+            >
                 <SheetHeader>
                     <SheetDescription>
-                        <div className="mt-10 flex flex-col gap-2">
-                            {navItems.map((navItem, index) => (
-                                <Link key={navItem.id} href={navItem.href}>
-                                    <SheetClose asChild>
-                                        <Button
-                                            variant={
-                                                pathname === navItem.href
-                                                    ? 'secondary'
-                                                    : 'ghost'
-                                            }
-                                            className="w-full justify-start"
-                                        >
-                                            {
-                                                <navItem.icon className="mr-2 h-4 w-4" />
-                                            }
-                                            {navItem.title}
-                                        </Button>
-                                    </SheetClose>
-                                </Link>
-                            ))}
-                        </div>
+                        {userId ? (
+                            <div className="mt-10 flex flex-col gap-2">
+                                {navItems.map((navItem, index) => (
+                                    <Link key={navItem.id} href={navItem.href}>
+                                        <SheetClose asChild>
+                                            <Button
+                                                variant={
+                                                    pathname === navItem.href
+                                                        ? 'secondary'
+                                                        : 'ghost'
+                                                }
+                                                className="w-full justify-start"
+                                            >
+                                                {
+                                                    <navItem.icon className="mr-2 h-4 w-4" />
+                                                }
+                                                {navItem.title}
+                                            </Button>
+                                        </SheetClose>
+                                    </Link>
+                                ))}
+                            </div>
+                        ) : (
+                            <SignedOut>
+                                <div className="space-y-2">
+                                    <SignInButton>
+                                        <SheetClose asChild>
+                                            <Button className="w-full">
+                                                Log In
+                                            </Button>
+                                        </SheetClose>
+                                    </SignInButton>
+
+                                    <SignUpButton>
+                                        <SheetClose asChild>
+                                            <Button
+                                                variant="outline"
+                                                className="w-full"
+                                            >
+                                                Sign Up
+                                            </Button>
+                                        </SheetClose>
+                                    </SignUpButton>
+                                </div>
+                            </SignedOut>
+                        )}
                     </SheetDescription>
                 </SheetHeader>
             </SheetContent>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,17 @@
+import { auth } from '@clerk/nextjs'
+
 import Auth from './auth'
 import MobileMenu from './mobile-menu'
 import { ModeToggle } from './theme/mode-toggle'
 
 export default function Navbar() {
+    const { userId } = auth()
+
     return (
         <div className="sticky top-0 z-10 border-b bg-background">
             <div className="flex h-16 items-center px-4">
                 <div className="md:hidden">
-                    <MobileMenu />
+                    <MobileMenu userId={userId ?? ''} />
                 </div>
 
                 <div className="ml-auto flex items-center space-x-4">
